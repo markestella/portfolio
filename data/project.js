f937cca7b3a9ff3635bc69e4d20f359bd3627ad5
Netzon Mark <mark.estella09@gmail.com>
2025-08-25 00:34:52 +0800

    chore: add next.js project

--- data/project.js
+++ data/project.js
@@ -30,9 +30,21 @@ const projects = [
     image: './assets/project_images/AI_Chatbot_Image.png',
     video: 'lxtQqa40R0g?si=lL-jD7YWaONBbKrd',
     description: 'A full-stack chatbot application showcasing a React + Vite frontend with ShadCN UI and TailwindCSS, connected to a .NET backend API with PostgreSQL and integrated with Gemini AI for intelligent chat responses. Demonstrates real-time chat interactions, clean UI, and backend functionality in a single project.',
-    tech: ['React + Vite', 'Typescript', 'C#', '.Net', 'Docker', 'PostgreSQL', 'Tailwind CSS', 'ShadcnUI', 'Tanstack Router', 'Gemini', 'Entity Frmework', 'JWT Authentication', 'Swagger/Swashbuckle', 'Github Actions'],
+    tech: ['React + Vite', 'Typescript', 'C#', '.Net', 'Docker', 'PostgreSQL', 'Tailwind CSS', 'ShadcnUI', 'Tanstack Router', 'Gemini', 'Entity Framework', 'JWT Authentication', 'Swagger/Swashbuckle', 'Github Actions', 'Vercel'],
     codeLink: 'https://github.com/markestella/ai-chat-full-stack',
     demoLink: 'https://full-stack-ai-demo.vercel.app/',
     category: 'Full Stack & AI'
+  },
+    {
+    id: 'recipe-finder',
+    title: 'Recipe Finder App',
+    date: 'Aug 2025',
+    image: './assets/project_images/Recipe_Finder.png',
+    video: 'O_D65GGTZVg?si=hgYgPDM4wjEBiZBV',
+    description: 'A modern recipe discovery app built with Next.js and Tailwind CSS. Search, view, and save your favorite recipes using TheMealDB API.',
+    tech: ['Next.js', 'Tailwind CSS', 'TheMealDBApi', 'Vercel'],
+    codeLink: 'https://github.com/markestella/recipe-finder',
+    demoLink: 'https://recipe-finder-demo-chi.vercel.app/',
+    category: 'Next.js'
   },
 ];
